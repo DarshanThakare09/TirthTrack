7a3f22aeefba256191c3e00f7b66e86cee339633
Darsh <darshanthakaree09@gmail.com>
2026-08-11 14:29:52 +0530

    admin_app init

---
+++ tirth_admin/README.md
@@ -0,0 +1,3 @@
+# tirth_admin
+
+A new Flutter project.
---
+++ tirth_admin/android/app/src/main/kotlin/com/example/tirth_admin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tirth_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
